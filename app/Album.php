a96c1d95ceba6db777e057befc0ff8e8f728e27b
Joseph_Li <Joseph_Li@asus.com>
2019-04-25 15:50:13 +0800

    [Laravel][musicshop][Model] Update fillable columns to allow mass assignment.

--- app/Album.php
+++ app/Album.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Album extends Model
 {
     //
+    protected $fillable=[
+        'album_name', 'singer', 'coverimg_file', 'release', 'price',
+    ];
 }
--- app/Cart.php
+++ app/Cart.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     //
+    protected $fillable = [
+        'number',
+    ];
 }
