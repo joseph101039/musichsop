4b322d785ab9d9453988ec7654f3302cfee47996
Joseph_Li <Joseph_Li@asus.com>
2019-04-26 13:47:57 +0800

    [Laravel][musicshop][View] Add navigate bar (menu) for each page.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,15 +2,67 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="utf-8">
+        <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+        <script>
+            $(function(){
+                $('a').each(function(){
+                    if($(this).prop('href') == window.location.href){
+                        $(this).addClass('active-nav'); 
+                        $(this).parent('li').addClass('active-nav');
+                    }
+                });
+            });
+        </script>
         <title>Music Shop</title>
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
 
         <style>
 
+        ul.navbar {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+ 
+        }
+
+        ul.navbar .active-nav{
+            color: #F8F8F8;
+            background-color: #4f81bd;
+        }
+
+        li.nav-item {
+        float: left;
+        }
+
+        li.right-nav-item {
+        float: right;
+        background-color: #555;
+        }
+        li.auto{
+            margin-right:auto!important;
+        }
+
+        li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 26px;
+ 
+        text-decoration: none;
+        }
+
+        li a:hover {
+        background-color: #111;
+        }
+
+
         </style>
     </head>
     <body>
         <div class="container">
+            @include('menu')
             @yield('content')
         </div>
     </body>
---
+++ resources/views/menu.blade.php
@@ -0,0 +1,45 @@
+<div class="nav-bar">
+    <nav>
+
+        <ul class="navbar">
+        @guest
+        <li class="nav-item">
+                <a class="nav-link" href="{{route('login')}}">LOGIN</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">REGISTER</a>
+            </li>
+            <li class="auto"></li>
+            <li class="right-nav-item"><a>Guest</a></li>
+        @else
+         
+            <li class="nav-item">
+                <a class="nav-link" href="/home">HOME</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/Browse">BROWSE</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/cart">CART</a>
+            </li>
+            <li class="auto"></li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('logout') }}"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                LOGOUT
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </li>
+            
+            
+            <li class="right-nav-item" style="float:right" ><a>{{ Auth::user()->name }} </a></li>
+
+        @endguest
+
+        </ul>
+       
+    </nav>
+
+</div>
