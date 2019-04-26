8772e4ffdb2a6cb354752a4fb5c3eee4732316fd
Joseph_Li <Joseph_Li@asus.com>
2019-04-26 13:48:07 +0800

    [Laravel][musicshop][Model] Set the dynamic eloquent relationship between user and cart.

--- app/Cart.php
+++ app/Cart.php
@@ -10,4 +10,11 @@ class Cart extends Model
     protected $fillable = [
         'number',
     ];
+    protected $table = 'cart';
+
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
